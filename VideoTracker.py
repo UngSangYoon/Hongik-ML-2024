344b4f42ace2e24dd08a8003af845489b818f0a2
UngSangYoon <124053569+UngSangYoon@users.noreply.github.com>
2024-06-08 23:46:50 +0900

    Update VideoTracker.py

--- VideoTracker.py
+++ VideoTracker.py
@@ -12,14 +12,26 @@ class VideoTracker:
         self.cls_name_list = []
         self.frame_num = 0
         self.fps = None
-
-    def process_video_stream(self):
+        self.width = None
+        self.height = None
+        self.frame_buffer = []
+        
+    def get_video_info(self):
         cap = cv2.VideoCapture(self.video_source)
         self.fps = round(cap.get(cv2.CAP_PROP_FPS))
+        self.width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        return self.fps, self.width, self.height
+    
+    def process_video_stream(self):
+        cap = cv2.VideoCapture(self.video_source)
+        interval = self.fps // 5  # 0.2초마다 처리
         total_frames_per_segment = 10 * self.fps  # Total frames to process for each 10-second segment
 
         while cap.isOpened():
             frame_count = 0
+            self.frame_buffer = []
+
             while frame_count < total_frames_per_segment:
                 success, frame = cap.read()
                 if not success:
@@ -28,8 +40,7 @@ class VideoTracker:
                 self.frame_num += 1
                 frame_count += 1
 
-                # Process frame every second
-                if self.frame_num % self.fps == 0:
+                if self.frame_num % interval == 0:
                     self.process_frame(frame)
 
             if frame_count == 0:
@@ -42,7 +53,7 @@ class VideoTracker:
 
     def process_frame(self, frame):
         frame_info = {}
-        results = self.model.track(frame, persist=True, show=True)
+        results = self.model.track(frame, persist=True, show=True, tracker="bytetrack.yaml")
         boxes = results[0].boxes.xywh.cpu()
 
         if results[0].boxes.id is not None:
@@ -50,6 +61,7 @@ class VideoTracker:
             ids_labels = results[0].boxes.cls.int().tolist()
 
             frame_info['frame_number'] = self.frame_num
+            frame_info['track_count'] = len(track_ids)
             for box, track_id, object_label in zip(boxes, track_ids, ids_labels):
                 object_info = {}
                 x, y, w, h = box
@@ -59,14 +71,20 @@ class VideoTracker:
                     self.id_class_history[track_id] = []
                 self.id_class_history[track_id].append(object_label)
             if frame_info:
-                self.track_history.append(frame_info)
+                self.frame_buffer.append(frame_info)
+            if len(self.frame_buffer) == 5:
+                best_frame = max(self.frame_buffer, key=lambda x: x['track_count'])
+                del best_frame['track_count']
+                self.track_history.append(best_frame)
+                self.frame_buffer = []
+
 
     def finalize_processing(self):
         self.cls_name_list = self.model.names
         self.add_object_name()
 
     def add_object_name(self):
-        for frame_data in self.track_history:
+        for frame_data in self.frame_buffer:
             for obj_id, obj_data in frame_data.items():
                 if obj_id != 'frame_number':
                     obj_id_int = int(obj_id)
@@ -78,7 +96,6 @@ class VideoTracker:
     def output_result(self):
         if self.track_history:
             dict_data = {
-                'size': (self.fps, self.frame_num),
                 'total_frame_count': self.frame_num,
                 'track_history': self.track_history
             }
@@ -86,7 +103,8 @@ class VideoTracker:
 
 # Example usage
 if __name__ == "__main__":
-    model_path = "./yolov9c_custom.pt"
-    video_source = 0  # Use 0 for webcam, or replace with video file path
+    model_path = "yolov8n_adamw.pt"
+    video_source = 0 # Use 0 for webcam, or replace with video file path
     tracker = VideoTracker(model_path, video_source)
+    tracker.get_video_info()
     tracker.process_video_stream()
